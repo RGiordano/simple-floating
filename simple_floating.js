0a0c1f6e94a12391b8cb0a3e9dcbb0670b1c0773
Raphael Giordano <raphaelgns@gmail.com>
2016-09-21 17:36:49 -0300

    jQuery compatibility and some fixes

--- simple_floating.js
+++ simple_floating.js
@@ -1,5 +1,5 @@
 /**
- Version: 0.1.1
+ Version: 0.1.6
  Author: Raphael Giordano Silva
  Website: http://rgiordano.github.io/simple-floating
  Docs: http://rgiordano.github.io/simple-floating
@@ -7,46 +7,38 @@
  Issues: http://github.com/RGiordano/simple-floating/issues
  */
 
-$.fn.simpleFloating = function(container,top) {
-    if($(this).length==0)
+jQuery.fn.simpleFloating = function(container,top) {
+    if(jQuery(this).length==0)
         return false;
 
-    if($(container).length==0)
+    if(jQuery(container).length==0)
         return false;
 
-    var bottom_wrap = ($(container).offset().top+$(container).height());
-    var $sidebar   = $(this),
-        $window    = $(window),
-        offset     = $sidebar.offset();
+    var bottom_wrap = (jQuery(container).offset().top+jQuery(container).height());
+    var sidebar   = jQuery(this),
+        offset     = sidebar.offset();
 
     if(typeof top === "undefined")
         var topPadding = 0;
     else
         var topPadding = top;
 
-    $window.scroll(function() {
-        bottom_wrap = ($(container).offset().top+$(container).height());
-        var bottom_side = ($sidebar.offset().top+$sidebar.height());
-        if ($window.scrollTop() > (offset.top-topPadding)) {
-            if(bottom_side < bottom_wrap) {
-                if(($window.scrollTop() - offset.top + topPadding) <= bottom_wrap - (offset.top+$sidebar.height())){
-                    var top = $window.scrollTop() - offset.top + topPadding;
-                }else{
-                    var top = bottom_wrap - (offset.top+$sidebar.height());
-                }
-                $sidebar.stop().animate({
-                    marginTop: top
-                });
-            }else{
-                if(($window.scrollTop() - offset.top + topPadding) <= bottom_wrap - (offset.top+$sidebar.height())){
-                    var top = $window.scrollTop() - offset.top + topPadding;
-                    $sidebar.stop().animate({
-                        marginTop: top
-                    });
-                }
-            }
+    jQuery(window).scroll(function() {
+        var screen_top = jQuery(window).scrollTop();
+        bottom_wrap = (jQuery(container).offset().top+jQuery(container).height());
+        var max_top = offset.top-topPadding;
+
+        if (jQuery(window).scrollTop() > max_top) {
+            var top = screen_top - jQuery(container).offset().top + topPadding;
+
+            if((top+sidebar.height()+offset.top) > (bottom_wrap))
+                top = bottom_wrap - sidebar.height() - jQuery(container).offset().top;
+
+            sidebar.stop().animate({
+                marginTop: top
+            });
         } else {
-            $sidebar.stop().animate({
+            sidebar.stop().animate({
                 marginTop: 0
             });
         }
